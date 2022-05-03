543025107831342bdbc99d8447974e52cbb55299
KshitijV97 <writetokshitij@gmail.com>
2022-05-03 11:56:38 +0530

    Made separate list component

---
+++ src/app/list/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function List({ items }) {
+	return (
+		<ul>
+			{items.map((item) => (
+				<li>
+					<div className='__dml_list-item'>
+						<a href={`https://youtube.com/watch?v=${item.ext_id}`}>
+							<img
+								src={`/thumbs/${item.ext_id}`.png}
+								width='100%'
+								alt={item.title}
+							/>
+							<div>{item.title}</div>
+						</a>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
