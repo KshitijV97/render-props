299a21c807d3f1448a4c7ed79c1d4ac629ca975e
KshitijV97 <writetokshitij@gmail.com>
2022-05-03 16:10:05 +0530

    Moved screencast logic to another component

---
+++ src/app/filter/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Filter({ render, placeholder }) {
+	const [query, setQuery] = useState('');
+	return (
+		<div>
+			<input
+				type='text'
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				placeholder={placeholder}
+			/>
+		</div>
+	);
+}
