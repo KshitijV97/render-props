cf4a396bb4af63d1f84949bcc41b1c66a3a68c08
KshitijV97 <writetokshitij@gmail.com>
2022-05-03 11:54:24 +0530

    Moved List logic in List component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import './styles.css';
-
+import List from './list';
 import screencasts from '../data/screencasts.json';
 
 export default function App() {
 	return (
 		<div>
-			<ul>
-				{screencasts.map((screencast) => (
-					<li>
-						<div className='__dml_list-item'>
-							<a
-								href={`https://youtube.com/watch?v=${screencast.ext_id}`}
-							>
-								<img
-									src={`/thumbs/${screencast.ext_id}`.png}
-									width='100%'
-									alt={screencast.title}
-								/>
-								<div>{screencast.title}</div>
-							</a>
-						</div>
-					</li>
-				))}
-			</ul>
+			<List items={screencasts} />
 		</div>
 	);
 }
