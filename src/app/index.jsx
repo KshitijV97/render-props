1e964147da7598c9cde826d41dd075fe7a452e31
KshitijV97 <writetokshitij@gmail.com>
2022-05-03 16:12:20 +0530

    Using render props pattern

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './styles.css';
 import List from './list';
 import screencasts from '../data/screencasts.json';
+import Screencast from './screencast';
+import Filter from './filter';
 
 export default function App() {
+	function filterScreencasts(query, screencasts) {
+		return (
+			screencasts
+				.filter((screencast) => screencast.title.toLowerCase())
+				.indexOf(query.toLowerCase()) > -1
+		);
+	}
+
 	return (
 		<div>
-			<List items={screencasts} />
+			<Filter
+				placeholder='Type to filter the screencasts'
+				render={(query) => (
+					<List
+						items={filterScreencasts(query, screencasts)}
+						id='ext_id'
+						render={(screencast) => (
+							<Screencast
+								ext_id={screencast.ext_id}
+								title={screencast.title}
+							/>
+						)}
+					/>
+				)}
+			/>
 		</div>
 	);
 }
